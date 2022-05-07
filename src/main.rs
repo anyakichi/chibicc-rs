bf0a2eefe54fa4b06bf29dc4b14370e3545a1f54
INAJIMA Daisuke <inajima@gmail.com>
2022-05-07 18:38:40 +0900

    Add + and - operators

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 use std::env;
 use std::str::FromStr;
 
+fn strtol(s: &str) -> Result<(i64, &str), Box<dyn std::error::Error>> {
+    if !s
+        .chars()
+        .nth(0)
+        .ok_or("invalid number".to_string())?
+        .is_digit(10)
+    {
+        return Err("invalid number".to_string()).map_err(|e| e.into());
+    }
+
+    match s.find(|c: char| !c.is_digit(10)) {
+        Some(i) => {Ok((i64::from_str(&s[..i])?, &s[i..]))},
+        None => Ok((i64::from_str(s)?, "")),
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
 
@@ -10,7 +26,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("  .globl main");
     println!("main:");
-    println!("  mov ${}, %rax", i64::from_str(&args[1])?);
+
+    let (mut n, mut s) = strtol(&args[1])?;
+    println!("  mov ${}, %rax", n);
+
+    while s.len() != 0 {
+        let op = match s.chars().next().unwrap() {
+            '+' => "add",
+            '-' => "sub",
+            other => panic!("unexpected character: {}", other)
+        };
+        (n, s) = strtol(&s[1..])?;
+        println!("  {} ${}, %rax", op, n)
+    }
+
     println!("  ret");
 
     Ok(())
